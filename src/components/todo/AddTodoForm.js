ad617f7a810b7b3cdf150c1d1866484ee66d5348
Prabhat Gautam <prabhatgautam@lftechnology.com>
2017-09-21 16:19:39 +0545

    Refactor

--- src/components/todo/AddTodoForm.js
+++ src/components/todo/AddTodoForm.js
@@ -12,6 +12,7 @@ class AddTodoForm extends Component {
   render() {
     return (
         <div className="form-elements">
+          <span className="todo-form-header">Add a new todo</span>
           <textarea type="text" onChange={(e) => this.setState({todo: e.target.value})}/>
           <button onClick={this.props.closeForm} className='cancel-button'>Cancel</button>
           <input className="submit-button" type="submit" onClick={() => this.props.addTodo(this.state.todo)}/>
